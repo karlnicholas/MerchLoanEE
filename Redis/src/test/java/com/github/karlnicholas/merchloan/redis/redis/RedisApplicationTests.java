d894adb5dedc0c6a74560ddb7a76757bcd164a5d
Karl Nicholas <karl.nicholas@outlook.com>
2022-03-26 18:05:40 -0400

    WIP: Statement and Closing flow rework, retry logic debug

--- Redis/src/test/java/com/github/karlnicholas/merchloan/redis/redis/RedisApplicationTests.java
+++ Redis/src/test/java/com/github/karlnicholas/merchloan/redis/redis/RedisApplicationTests.java
@@ -3,6 +3,7 @@ package com.github.karlnicholas.merchloan.redis.redis;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class RedisApplicationTests {
 
     @Test
