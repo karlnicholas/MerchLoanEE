a010d20abbce9440eaf9e43760f7c95feacaa433
Karl Nicholas <karl.nicholas@outlook.com>
2022-04-17 18:50:30 -0400

    Docker, client 10000 test, 6K msg/sec peak rabbitmq

--- Redis/src/main/java/com/github/karlnicholas/merchloan/redis/config/RedisConfig.java
+++ Redis/src/main/java/com/github/karlnicholas/merchloan/redis/config/RedisConfig.java
@@ -1,8 +1,11 @@
 package com.github.karlnicholas.merchloan.redis.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 import java.time.LocalDate;
@@ -10,11 +13,27 @@ import java.time.LocalDate;
 @Configuration
 public class RedisConfig {
 
+    private @Value("${redis.host:localhost}") String redisHost;
+    private @Value("${redis.port:6379}") int redisPort;
+
     @Bean
-    public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory connectionFactory) {
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+    @Bean
+    public LettuceConnectionFactory connectionFactory() {
+
+        RedisStandaloneConfiguration redisConf = new RedisStandaloneConfiguration();
+        redisConf.setHostName(redisHost);
+        redisConf.setPort(redisPort);
+
+        return new LettuceConnectionFactory(redisConf);
+    }
+
+    @Bean
+    RedisTemplate<?, ?> redisTemplate() {
         RedisTemplate<Long, LocalDate> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
-        // Add some specific configuration here. Key serializers, etc.
+        template.setConnectionFactory( connectionFactory() );
         return template;
     }
 }
