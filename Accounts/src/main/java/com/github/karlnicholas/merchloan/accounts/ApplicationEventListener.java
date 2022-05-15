de9e270e8468f24f8f2e5e7acad9cd210c6461c2
Karl Nicholas <karl.nicholas@outlook.com>
2022-05-14 22:13:01 -0400

    Some Listener

--- Accounts/src/main/java/com/github/karlnicholas/merchloan/accounts/ApplicationEventListener.java
+++ Accounts/src/main/java/com/github/karlnicholas/merchloan/accounts/ApplicationEventListener.java
@@ -1,17 +1,16 @@
 package com.github.karlnicholas.merchloan.accounts;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class ApplicationEventListener implements ApplicationListener<ApplicationEvent> {
     @Override
     public void onApplicationEvent(ApplicationEvent applicationEvent) {
-        System.out.println(applicationEvent);
-//        if ( applicationEvent instanceof ContextClosedEvent) {
-//            System.out.println(applicationEvent);
-//        }
+        log.info("Event: {}", applicationEvent);
     }
 }
