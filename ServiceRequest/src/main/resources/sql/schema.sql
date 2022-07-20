98be292d09f43521315a3f80ca8972b12c291fed
Karl Nicholas <karl.nicholas@outlook.com>
2022-07-20 09:40:03 -0400

    mysql

--- ServiceRequest/src/main/resources/sql/schema.sql
+++ ServiceRequest/src/main/resources/sql/schema.sql
@@ -1,2 +1,2 @@
 drop table if exists service_request;
-create table service_request (id BINARY(16) not null, local_date_time timestamp, request clob, request_type varchar(1000), retry_count integer, status integer, status_message varchar(10000), primary key (id))
+create table service_request (id BINARY(16) not null, local_date_time timestamp, request text, request_type varchar(1000), retry_count integer, status integer, status_message varchar(10000), primary key (id))
